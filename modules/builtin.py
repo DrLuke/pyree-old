d926a7dfb6c6c8b7c59dfec9fd6144ebd20fb6f6
DrLuke <Lukas@Jackowski.de>
2016-05-03 18:28:37 +0200

    Add builtin nodes module

---
+++ modules/builtin.py
@@ -0,0 +1,54 @@
+from baseModule import BaseNode, Pin
+
+__nodes__ = ["Loop", "Init"]
+
+class Loop(BaseNode):
+    nodeName = "drluke.builtin.Loop"
+    name = "Loop"
+    desc = "Beginning of loop"
+    placable = False
+
+    def init(self):
+        pass
+
+    def run(self):
+        print(" --")
+        print("Loop starting!")
+        self.fireExec(0)
+
+    def getExecdata(self):
+        pass
+
+    inputDefs = [
+    ]
+
+    outputDefs = [
+        Pin("exec", "exec", getExecdata),
+    ]
+
+class Init(Loop):
+    nodeName = "drluke.builtin.Init"
+    name = "Init"
+    desc = "Beginning of init"
+
+    def init(self):
+        print("Init created")
+
+    def run(self):
+        print("Init running")
+
+class SheetInput(BaseNode):
+    nodeName = "drluke.builtin.SheetInput"
+    name = "Sheet Input"
+    desc = "Input for the sheet. Will be read out once every loop."
+    placable = False
+
+    def getInput(self):
+        pass
+
+    # TODO: Figure out dynamic pincount and typing
+    inputDefs = [
+    ]
+
+    outputDefs = [
+    ]

39bd60bd0d2f9e44228345d7105f488facf5bf49
DrLuke <Lukas@Jackowski.de>
2016-05-05 19:22:18 +0200

    Update worker handler

--- workerhandler.py
+++ workerhandler.py
@@ -1,7 +1,9 @@
 from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QTreeWidgetItem
 
 import socket
 from select import select
+import re
 
 class Connection():
     def __init__(self, ip, port, callback):
@@ -9,10 +11,13 @@ class Connection():
         self.port = port
         self.callback = callback
 
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.ip, self.port))
-
         self.valid = True
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.socket.connect((self.ip, self.port))
+        except ConnectionRefusedError:
+            self.valid = False
 
         self.outbuf = ""
 
@@ -46,14 +51,22 @@ class Worker:
     def messagecallback(self, message):
         self.inbuf += message
 
+        print(self.inbuf)
+
+    def createTreeitem(self):
+        self.workerTreeItem = QTreeWidgetItem()
+        self.workerTreeItem.setText(0, str(self.connection.ip) + ":" + str(self.connection.port))
+        self.parent.workerDockWidget.workerTree.addTopLevelItem(self.workerTreeItem)
+
+    def __del__(self):
+        self.parent.workerDockWidget.workerTree.invisibleRootItem().removeChild(self.workerTreeItem)
 
 class WorkerHandler():
-    def __init__(self, connectionHandler, workerDockWidget):
-        self.connectionHandler = connectionHandler
+    def __init__(self, workerDockWidget):
         self.workerDockWidget = workerDockWidget
 
         self.connections = {}
-        self.workers = {}
+        #self.workers = {}
 
         self.inbuf = ""
 
@@ -61,30 +74,50 @@ class WorkerHandler():
         self.timer.timeout.connect(self.tick)
         self.timer.start(20)
 
+        self.workerDockWidget.newConnButton.clicked.connect(self.buttonClicked)
+
+
+
+    def buttonClicked(self):
+        newIp = self.workerDockWidget.newConnCombobox.currentText()
+        if newIp:
+            if ":" in newIp:
+                match = re.search("(.*?):([\d]{1,8})", newIp)
+                self.newWorker(match.group(1), match.group(2))
+            else:
+                self.newWorker(newIp, 31337)
+
     def newWorker(self, ip, port):
-        self.workers[str(ip) + ":" + str(port)] = Worker(self)
-        newconn = Connection(ip, port)
-        self.workers[str(ip) + ":" + str(port)].connection = newconn
-        self.connections[newconn.socket] = newconn
+        newWorker = Worker(self)
+        newconn = Connection(ip, port, newWorker.messagecallback)
+        newWorker.connection = newconn
+
+        if newconn.valid:
+            newWorker.createTreeitem()  # TODO: Move this into worker, let it create this itself once "ok" response is recvd
+            #self.workers[str(ip) + ":" + str(port)] = newWorker
+            self.connections[newconn.socket] = (newconn, newWorker)
 
     def tick(self):
         # Kill dead connections
-        for key in self.connections:
-            if not self.connections[key].valid:
+        for key in dict(self.connections):
+            if not self.connections[key][0].valid:
+                self.connections[key][0].callback = None    # Remove references to clear worker and connection object for deletion
+                self.connections[key][1].connection = None
                 del self.connections[key]
+                continue
 
         # Get all sockets to read from
-        rlist = [x.socket for x in list(self.connections.values())]
+        rlist = [x[0].socket for x in list(self.connections.values())]
         # Get all sockets with non-empty write buffer
-        wlist = [x.socket for x in list(self.connections.values()) if x.outbuf]
+        wlist = [x[0].socket for x in list(self.connections.values()) if x[0].outbuf]
 
         # Run select to check if there's anything to read or if we can write
         rlist, wlist, elist = select(rlist, wlist, wlist, 0)
         for sock in rlist:
-            self.connections[sock].sockrecv()
+            self.connections[sock][0].sockrecv()
 
         for sock in wlist:
-            self.connections[sock].socksend()
+            self.connections[sock][0].socksend()
 
         # Print errors
         if elist:
